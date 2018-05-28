7e748c0de8106cbf6fbf899df4ee6d8fe85fadb6
Dylan-Hamel <dylan.hamel@heig-vd.ch>
2018-05-28 20:33:28 +0000

    Partie 3 - Reverse Proxy Done

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -4,13 +4,8 @@ var chance = new Chance();
 var express = require('express');
 var app = express();
  
-app.get('/dh', function(request, respond) {
-	respond.send("SEND DH !! ");
-
-});
-
 app.get('/', function(request, respond) {
-	respond.send("SEND BASE!! ");
+	respond.send(generateStudents());
 
 });
 
@@ -18,4 +13,16 @@ app.listen(3000, function() {
 	console.log("Accept HTTP requests");
 });
 
-// console.log("Bonjour " + chance.name());
+
+function generateStudents() {
+ 	var numberOfStudents = chance.integer({min:0,max:10});
+	console.log(numberOfStudents);
+	var students = [];
+	for (var i=0;i<numberOfStudents;i++) {
+		var gender = chance.gender();
+		var birthYear = chance.year({min:1986,max:1996});
+		students.push({fistName:chance.first({gender: gender}), lastname:chance.last(),gender:gender,birthday:chance.birthday({year: birthYear})});
+	};
+	console.log(students);
+	return students;
+}
