7d60fd44a1a5408c07355c8985f33d9f81b37f11
Dylan-Hamel <dylan.hamel@heig-vd.ch>
2018-06-06 17:28:42 +0000

    Load Balancing et Reverse Proxy OK

--- docker-images/rp-lb/template/config-template.php
+++ docker-images/rp-lb/template/config-template.php
@@ -6,40 +6,32 @@
 ?>
 
 <VirtualHost *:80>
-	ProxyRequests off
         ServerName demo.res.ch
 
-	<Proxy balancer://apache-php>
+	<Proxy "balancer://apache-php">
 		# Static
-                BalancerMember 'http://<?php print "$ip_static"?>/'
+                BalancerMember 'http://<?php print "$ip_static"?>'
 		# Static
-                BalancerMember 'http://<?php print "$ip_static2"?>/'
+                BalancerMember 'http://<?php print "$ip_static2"?>'
 	</Proxy>
 
 
-	<Proxy balancer://express-dyn>
+	<Proxy "balancer://express-dyn">
                 # Dynamic
-                BalancerMember 'http://<?php print "$ip_dyn"?>/'
+                BalancerMember 'http://<?php print "$ip_dyn"?>'
 		# Dynamic
-                BalancerMember 'http://<?php print "$ip_dyn2"?>/'
+                BalancerMember 'http://<?php print "$ip_dyn2"?>'
         </Proxy>
-
-
-	<Location "/balancer-manager">
-		SetHandler balancer-manager
-		Require host demo.res.ch
-	</Location>
-
 	
-
-        #ErrorLog ${APACHE_LOG_DIR}/error.log
-        #CustomLog ${APACHE_LOG_DIR}/access.log combined
-
 	ProxyPass "/api/students/" "balancer://express-dyn"
 	ProxyPassReverse "/api/students/" "balancer://express-dyn"
 
-	ProxyPass "/" "balancer:/apache-php"
+	ProxyPass "/" "balancer://apache-php"
 	ProxyPassReverse "/" "balancer://apache-php"
 
+        <Location "/balancer-manager">
+                SetHandler balancer-manager
+                Require host demo.res.ch
+        </Location>
 
 </VirtualHost>
